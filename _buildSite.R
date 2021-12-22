3c4833613152cfaa6fc07c491322ef950ae86a93
Rodney <rjdyer@vcu.edu>
2021-12-22 09:49:32 -0500

    working on automatic updates for software packages

--- _buildSite.R
+++ _buildSite.R
@@ -41,6 +41,55 @@ for( file in list.files("_toImport", pattern = "*.md", full.names = TRUE) ) {
   rmarkdown::render( new_file )
 }
 
+
+
+
+# Create software packages for the latest versions
+
+
+
+readme <- c("https://raw.githubusercontent.com/dyerlab/DLabSpatial/main/README.md")
+title <- c("DLabSpatial Swift Package")
+description <- c("A swift library package for the analysis and display of spatial data for analytical software targeting iOS and MacOS platforms.")
+categories <- c("Swift")
+
+
+
+curl -s "https://api.github.com/repos/user/project-name/commits?path=filename&page=1&per_page=1" | jq -r '.[0].commit.committer.date'
+
+yml() %>% 
+  yml_title( title ) %>% 
+  yml_author( name="Rodney Dyer", 
+            email = "rjdyer@vcu.edu",
+            url = "https://dyerlab.org",
+            affiliation = "Center for Environmental Studies",
+            affiliation_url = "https://ces.vcu.edu",
+            orcid_id = "0000-0003-4707-3453") %>%
+  yml_distill_opts( categories=categories ) -> txt 
+
+
+txt$output = "distill::distill_article"
+
+txt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # clean up any of the crap on the site and render the whole thing.
 system( "find . -type f -iname '.DS_Store' -delete")
 rmarkdown::render_site(".")
